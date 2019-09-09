1000d4439a6c4b998365373a7fa24acb7f0b780f
Philipp L <philipp.lissek@gmail.com>
2019-09-09 16:57:12 +0200

    added basic_gs.glsl to geoshader branch

---
+++ All_Code/Opengl_Camera_Finder/basic_gs.glsl
@@ -0,0 +1,22 @@
+#version 330 core
+
+layout(triangles) in;
+
+
+in vec4 g_color;
+in vec2 g_texCoord;
+in vec3 g_normal;
+in vec3 g_position;
+
+
+out vec4 v_color;
+out vec2 v_texCoord;
+out vec3 v_normal;
+out vec3 v_position;
+
+void main(){
+v_color = g_color;
+v_texCoord=g_texCoord;
+v_normal=g_normal;
+v_position=g_position;
+}

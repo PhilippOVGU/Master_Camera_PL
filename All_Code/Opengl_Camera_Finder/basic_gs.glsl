1eb30ce3d50ebae6f706c9b5f002bd102e93027e
Philipp L <philipp.lissek@gmail.com>
2019-10-10 18:26:14 +0200

    added simplest geoshader possible

--- All_Code/Opengl_Camera_Finder/basic_gs.glsl
+++ All_Code/Opengl_Camera_Finder/basic_gs.glsl
@@ -3,71 +3,54 @@ layout(lines_adjacency) in;
 //layout(triangle_strip, max_vertices = 20) out;
 layout(line_strip, max_vertices = 20) out;
 
-in vec4 v_color[];
-in vec2 v_texCoord[];
-in vec3 v_normal[]; 
-in vec3 v_position[];
 
+in vec4 g_color[];
+in vec2 g_texCoord[];
+in vec3 g_normal[];
+in vec3 g_position[]; 
 
-out vec4 g_color;
-out vec2 g_texCoord;
-out vec3 g_normal; 
-out vec3 g_position;
+out vec4 v_color;
+out vec2 v_texCoord;
+out vec3 v_normal; 
+out vec3 v_position;
+
 
 void main(){
     
-	/**
-	 * Nicht zu viele Sachen auf einmal! Ich habe den Geometry Shader mal so angepasst, dass er die existierende
-	 * Geometrie durchreicht (also auch Linien anstelle von Dreiecken produziert). Adaptiere das am besten in kleinen
-	 * Schritten. Den Umschwung von Linien auf Dreiecke würde ich erst am Ende machen - du kannst zwar Dreiecke berechnen,
-	 * diese aber trotzdem weiter als Linien ausgeben.
-	 */
-	
-	g_color = v_color[0];
-	g_texCoord = v_texCoord[0];
-	g_position = v_position[0];
-	g_normal = v_normal[0];
-	gl_Position = gl_in[0].gl_Position;
-	EmitVertex(); 
-	
-	g_color = v_color[1];
-	g_texCoord = v_texCoord[1];
-	g_position = v_position[1];
-	g_normal = v_normal[1];
-	gl_Position = gl_in[1].gl_Position;
-	EmitVertex(); 
-	
-	EndPrimitive();
-	
-	g_color = v_color[1];
-	g_texCoord = v_texCoord[1];
-	g_position = v_position[1];
-	g_normal = v_normal[1];
-	gl_Position = gl_in[1].gl_Position;
-	EmitVertex(); 
-	
-	g_color = v_color[2];
-	g_texCoord = v_texCoord[2];
-	g_position = v_position[2];
-	g_normal = v_normal[2];
-	gl_Position = gl_in[2].gl_Position;
-	EmitVertex(); 
-	
-	EndPrimitive();
-	
-	g_color = v_color[2];
-	g_texCoord = v_texCoord[2];
-	g_position = v_position[2];
-	g_normal = v_normal[2];
-	gl_Position = gl_in[2].gl_Position;
-	EmitVertex(); 
-	
-	g_color = v_color[3];
-	g_texCoord = v_texCoord[3];
-	g_position = v_position[3];
-	g_normal = v_normal[3];
-	gl_Position = gl_in[3].gl_Position;
-	EmitVertex(); 
-	
-	EndPrimitive();
+v_color=g_color[0];
+v_texCoord=g_texCoord[0];
+v_normal=g_normal[0]; 
+v_position=g_position[0];
+EmitVertex();
+v_color=g_color[1];
+v_texCoord=g_texCoord[1];
+v_normal=g_normal[1]; 
+v_position=g_position[1];
+EmitVertex();
+v_color=g_color[2];
+v_texCoord=g_texCoord[2];
+v_normal=g_normal[2]; 
+v_position=g_position[2];
+EmitVertex();
+v_color=g_color[3];
+v_texCoord=g_texCoord[3];
+v_normal=g_normal[3]; 
+v_position=g_position[3];
+EmitVertex();
+//v_color=g_color[4];
+//v_texCoord=g_texCoord[4];
+//v_normal=g_normal[4]; 
+//v_position=g_position[4];
+//EmitVertex();
+//v_color=g_color[5];
+//v_texCoord=g_texCoord[5];
+//v_normal=g_normal[5]; 
+//v_position=g_position[5];
+//EmitVertex();
+//v_color=g_color[6];
+//v_texCoord=g_texCoord[6];
+//v_normal=g_normal[6]; 
+//v_position=g_position[6];
+EmitVertex();
+EndPrimitive();
 }
